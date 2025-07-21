8ec4bf9c4264848043a1bdae807fc6ed03e3db5d
Huy Tran <baohuy82.th@gmail.com>
2025-07-21 09:24:33 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
 console.log("Hello, World!")
 console.log("This is a test script.")
+
+const user = {
+  name: "Alice",
+  age: 30,
+  address: "Wonderland",
+}
+
+const { name, age, address } = user
