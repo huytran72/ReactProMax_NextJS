ac4de5e07420c50bd9a9fa347d4f8b17fe072fed
Huy Tran <baohuy82.th@gmail.com>
2025-07-29 08:01:28 -0700

    Update App.tsx

--- react-vite-starter-master/src/App.tsx
+++ react-vite-starter-master/src/App.tsx
@@ -3,6 +3,10 @@ import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
 
+const MyFirstComponent = () => {
+  return <div></div>
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
