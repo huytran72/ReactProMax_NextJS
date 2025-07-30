22b950807cede533d710c1c5b29f13dbb25a1bf9
Huy Tran <baohuy82.th@gmail.com>
2025-07-30 14:15:34 -0700

    Create input.todo.tsx

---
+++ react-vite-starter-master/src/todo/input.todo.tsx
@@ -0,0 +1,11 @@
+const InputTodo = () => {
+  return (
+    <div>
+      <h2>Add a New Todo</h2>
+      <input type="text" placeholder="Add a new todo" />
+      <button>Add Todo</button>
+    </div>
+  )
+}
+
+export default InputTodo
