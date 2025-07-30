2029b632dcad0c78c1ecab3c2361eab73c18d5b6
Huy Tran <baohuy82.th@gmail.com>
2025-07-30 13:53:31 -0700

    Create second.component.tsx

---
+++ react-vite-starter-master/src/test/second.component.tsx
@@ -0,0 +1,19 @@
+const SecondComponent = () => {
+  // jsx syntax: html + js
+  return (
+    <div>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        class="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights </li>
+        <li>Rehearse a movie scene </li>
+        <li>Improve the spectrum technology </li>
+      </ul>
+    </div>
+  )
+}
+export default SecondComponent
