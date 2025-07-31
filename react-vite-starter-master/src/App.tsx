ebf32b3b18f84c9a2bbb77339b1221fccdf07d04
Huy Tran <baohuy82.th@gmail.com>
2025-07-31 09:18:53 -0700

    Update App.tsx

--- react-vite-starter-master/src/App.tsx
+++ react-vite-starter-master/src/App.tsx
@@ -3,6 +3,10 @@ import InputTodo from "./todo/input.todo"
 function App() {
   const name = "React Vite Starter"
   const age = 3
+  const info = {
+    gender: "male"
+    address: "123 Main St"
+  }
 
   return (
     <>
