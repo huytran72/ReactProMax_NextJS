f66eed62c9262c1ef7953c06e10fcacfba149326
Huy Tran <baohuy82.th@gmail.com>
2025-07-31 09:17:19 -0700

    Update App.tsx

--- react-vite-starter-master/src/App.tsx
+++ react-vite-starter-master/src/App.tsx
@@ -1,7 +1,15 @@
 import InputTodo from "./todo/input.todo"
 
 function App() {
-  return <InputTodo />
+  const name = "React Vite Starter"
+  const age = 3
+
+  return (
+    <>
+      <div className="app"></div>
+      <InputTodo />
+    </>
+  )
 }
 
 export default App
