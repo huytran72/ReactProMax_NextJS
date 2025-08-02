287e2307817e5c4efcae28b5e5642002834681a3
Huy Tran <baohuy82.th@gmail.com>
2025-08-02 10:19:21 -0700

    Update input.todo.tsx

--- react-vite-starter-master/src/todo/input.todo.tsx
+++ react-vite-starter-master/src/todo/input.todo.tsx
@@ -17,7 +17,7 @@ const InputTodo = (props: IProps) => {
       <div>age = {props.age}</div>
       <div>name = {props.name}</div>
       <h2>Add a New Todo</h2>
-      <input type="text" placeholder="Add a new todo" />
+      <input type="text" placeholder="Add a new todo" onChange={() => {}} />
       <button onClick={() => handleClick()}>Save</button>
     </div>
   )
