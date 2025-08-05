549e8441cba631547177952a769d862e37586314
Huy Tran <baohuy82.th@gmail.com>
2025-08-04 21:24:29 -0700

    Update App.tsx

--- react-vite-starter-master/src/App.tsx
+++ react-vite-starter-master/src/App.tsx
@@ -10,8 +10,8 @@ function App() {
 
   const todos = ["todo 1", "todo 2", "todo 3", "todo 4", "todo 5"]
 
-  const handleTest = () => {
-    alert("Test function called")
+  const handleTest = (name: string) => {
+    alert(`Test function called with name: ${name}`)
   }
 
   return (
