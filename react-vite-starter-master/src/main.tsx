ed2d219dd412f3dc31040e03071d3775742d60e4
Huy Tran <baohuy82.th@gmail.com>
2025-08-10 13:18:57 -0700

    Update main.tsx

--- react-vite-starter-master/src/main.tsx
+++ react-vite-starter-master/src/main.tsx
@@ -8,6 +8,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
   },
+  {
+    path: "/users",
+    element: <App />,
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
