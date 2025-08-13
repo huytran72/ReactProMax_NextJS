1be3807bd968c83d60459935bbe98c1a4ce3747d
Huy Tran <baohuy82.th@gmail.com>
2025-08-13 13:08:32 -0700

    Update users.table.tsx

--- react-vite-starter-master/src/components/users/users.table.tsx
+++ react-vite-starter-master/src/components/users/users.table.tsx
@@ -16,7 +16,7 @@ const UsersTable = () => {
 
   const getData = async () => {
     const access_token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjg5OGY5MmIwOTdjN2QwMDJhY2UxYzY5IiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3NTQ4NTg2MzIsImV4cCI6MTg0MTI1ODYzMn0.qOvU4AwFEL3NgFeVYV6jSLRFx8mPPBY7pk4D-pvIxwA"
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjg5Y2YwMzg5NWFhMWYwOWVjYThiODEzIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3NTUxMTU2MzAsImV4cCI6MTg0MTUxNTYzMH0.Ch30O1_Sv6jOlvf2pH67NdzPqGSGJfIQdn8YpncV-s0"
 
     const res = await fetch("http://localhost:8000/api/v1/users/all", {
       headers: {
