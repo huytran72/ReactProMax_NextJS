7e84aa97ceba7a76c0b0328b592d3ab950e21a95
Huy Tran <baohuy82.th@gmail.com>
2025-08-19 01:14:38 -0700

    Update users.table.tsx

--- react-vite-starter-master/src/components/users/users.table.tsx
+++ react-vite-starter-master/src/components/users/users.table.tsx
@@ -147,12 +147,35 @@ const UsersTable = () => {
     },
   ]
 
-  const handleOnchange = (page: number, pageSize: number) => {
+  const handleOnchange = async (page: number, pageSize: number) => {
     setMeta({
       current: page,
       pageSize: pageSize,
       pages: meta.pages,
       total: meta.total,
+    })
+    const res = await fetch(
+      `http://localhost:8000/api/v1/users?current=${page}&pageSize=${pageSize}`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    const d = await res.json()
+    if (!d.data) {
+      notification.error({
+        message: "Error",
+        description: JSON.stringify(d.message),
+      })
+    }
+    setListUsers(d.data.result)
+    setMeta({
+      current: d.data.meta.current,
+      pageSize: d.data.meta.pageSize,
+      pages: d.data.meta.pages,
+      total: d.data.meta.total,
     })
   }
 
