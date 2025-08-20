6599b2cd1c8a9e63a4714ef81c2f015383bc8d7f
Huy Tran <baohuy82.th@gmail.com>
2025-08-19 19:57:01 -0700

    Update create.user.modal.tsx

--- react-vite-starter-master/src/components/users/create.user.modal.tsx
+++ react-vite-starter-master/src/components/users/create.user.modal.tsx
@@ -87,10 +87,10 @@ const CreateUserModal = (props: IProps) => {
         name="basic"
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
         initialValues={{ remember: true }}
         onFinish={onFinish}
         autoComplete="off"
+        layout="vertical"
       >
         <Form.Item
           label="Username"
